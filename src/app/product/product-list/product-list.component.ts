9fc16263582c4787d8ef53a82500784003851a54
Cesar Andres Ramirez Garcia <andgg12345@gmail.com>
2024-09-23 20:35:24 -0600

    Tercer commit lunes 23 filtro

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
 
 @Component({
   selector: 'app-product-list',
@@ -10,43 +11,44 @@ export class ProductListComponent {
   imageWith:number=50;
   imageMargin:number=2;
   muestraImg:boolean=true;
+  listFiter:string='';
 
   mostrarImagen():void{
     this.muestraImg=!this.muestraImg
   }
 
-  productos: any[] = [
+  productos: IProductos[] = [
       {
-          "productoId": 1,
-          "modelo": "rubicon",
-          "descripcion": "4 puertas",
-          "year": "enero 15 2020",
-          "precio": 1200000,
-          "marca": "jeep",
-          "color": "negro",
-          "imgUrl": "https://img.remediosdigitales.com/511ec0/jeep-r-wrangler-unlimited-rubicon-edicion-deluxe-2020_2-1-/1366_2000.jpg"
+          "ProductoId": 1,
+          "Modelo": "sentra",
+          "Descripcion": "2 puertas",
+          "Year": "marzo 12 2021",
+          "Precio": 120000,
+          "Marca": "nisan",
+          "Color": "blanco",
+          "ImgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcBB5_JDrpahQJzQ4Y6U0HwfHS1wFHdUZ74w&s"
       
       },
       {
-          "productoId": 2,
-          "modelo": "vento",
-          "descripcion": "4 puertas",
-          "year": "mayo 2 2020",
-          "precio": 180000,
-          "marca": "volsvagen",
-          "color": "azul",
-          "imgUrl": "https://img.remediosdigitales.com/6f6fdc/volkswagen-vento-allstar-2017-mexico-2-/1366_2000.jpg"
+          "ProductoId": 2,
+          "Modelo": "a4",
+          "Descripcion": "2 puertas",
+          "Year": "marzo 12 2021",
+          "Precio": 130000,
+          "Marca": "volsvagen",
+          "Color": "blanco",
+          "ImgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg3hro7olm8DX8_jWsGyn0Aau9cxi73h7r-w&s"
       
       },
       {
-          "productoId": 3,
-          "modelo": "tsuru",
-          "descripcion": "4 puertas",
-          "year": "marzo 12 2021",
-          "precio": 30000,
-          "marca": "nissan",
-          "color": "blanco",
-          "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgEP_vIKAg18pDQ8OTBGyMWgeKJGlqshvs0A&s"
+          "ProductoId": 3,
+          "Modelo": "a5",
+          "Descripcion": "2 puertas",
+          "Year": "marzo 12 2021",
+          "Precio": 150000,
+          "Marca": "chevrolet",
+          "Color": "blanco",
+          "ImgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHz_1KZ194hzPMoqOCcN9rx4VnCm7OwkpLJQ&s"
       
       },
       
